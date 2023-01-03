c2010bc6baf98250dcc416b778eb72f322355ab3
Supriya Ashok Kale <102290355+supriyakale81@users.noreply.github.com>
2023-01-03 15:57:44 +0530

    Add files via upload

---
+++ Basic Query.sql
@@ -0,0 +1,30 @@
+select (10+20) as addition;
+select (10-20) as substraction;
+select len('India') as Total_length;
+select len('I Am An Unique') as Total_length;
+
+select UPPER('India');
+select lower('INDIA');
+
+select getdate();
+select day(GETDATE());
+
+select CONCAT('India',' is',' in',' Asia')as merged
+
+select REVERSE('India');
+
+select replace('Orange is a vegeatable','vegeatable','fruit'); 
+
+select len('        India      ');
+
+select ltrim('        India      ');
+select len(ltrim('        India      '));
+
+select trim('        India      ');
+
+select ascii('a');
+select ascii('A');
+select ascii(4);
+
+
+
